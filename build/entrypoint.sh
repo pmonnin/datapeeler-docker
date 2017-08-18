cff9181c7bef305c4bf218d3685b656123680e02
Pierre Monnin <pier.monn@gmail.com>
2017-08-18 18:13:08 +0200

    Improve image documentation

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -2,11 +2,14 @@
 
 if [ $# -eq 0 ]
 then
-	echo "=== Help from datapeeler-docker"
-	echo "Usage: docker run --rm dpeeler:latest [--getscript|DataPeeler options]"
+	echo  -e "=== Help from datapeeler-docker\n"
+	echo "Usage: docker run --rm qlf-sesi-harbor.inria.fr/orpailleur/dpeeler:latest [--getscript|DataPeeler options]"
 	echo -e "--getscript\t\treturn the script that can be used to call DataPeeler thanks to the Docker image"
-	echo -e "DataPeeler options\tSee DataPeeler help below\n\n"
-	echo "=== Help from DataPeeler"
+	echo -e "DataPeeler options\tSee DataPeeler help below\n"
+	echo -e "Example:\ncopy-paste this line to get the starting script, named datapeeler.sh"
+	echo "docker run --rm qlf-sesi-harbor.inria.fr/orpailleur/dpeeler:latest --getscript > datapeeler.sh && chmod 755 datapeeler.sh"
+	echo ""
+	echo -e "=== Help from DataPeeler\n"
 	d-peeler
 	exit 0
 elif [ $# -eq 1 ] && [ $1 = "--getscript" ]
